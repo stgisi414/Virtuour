758da9b0820930471f83dc623845be073158d23a
Stefan Gisi <141892560+stgisi414@users.noreply.github.com>
2025-06-17 07:17:57 +0000

    stuff

--- main.js
+++ main.js
@@ -87,12 +87,12 @@ function updateLocalTime(element) {
 }
 
 function getDistanceInKm(latLng1, latLng2) {
-    const R = 6371;
-    const dLat = (latLng2.lat() - latLng1.lat()) * Math.PI / 180;
-    const dLon = (latLng2.lng() - latLng1.lng()) * Math.PI / 180;
+    const R = 6371; // Radius of the Earth in km
+    const dLat = (latLng2.lat - latLng1.lat) * Math.PI / 180;
+    const dLon = (latLng2.lng - latLng1.lng) * Math.PI / 180;
     const a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(latLng1.lat() * Math.PI / 180) * Math.cos(latLng2.lat() * Math.PI / 180) *
+        Math.cos(latLng1.lat * Math.PI / 180) * Math.cos(latLng2.lat * Math.PI / 180) *
         Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     return R * c;
